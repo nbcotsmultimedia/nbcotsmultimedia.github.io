d32e1d7cd889985b3ed0d413422b0a9d6f4476ba
Stogniew <stogniew.a@northeastern.edu>
2023-12-11 15:11:55 -0500

    year in review

---
+++ year-in-review/js/yir.js
@@ -0,0 +1,198 @@
+let allData = [];
+let cardsToAdd = [];
+let config,
+	totalEntries,
+	numCols;
+const mobile = window.innerWidth <= 768;
+let imageGallery = $('#image-gallery');
+let dataFilter = "all";
+
+
+function init() {
+	buildColumns();
+	config = buildConfig();
+	loadData();
+};
+
+const buildColumns = () => {
+	numCols = mobile ? 2 : 3;
+	let colHtml = '';
+	for (let i = 0; i < numCols; i++) {
+		colHtml += `<div id="col-${i}" class="column"></div>`;
+	}
+	imageGallery.append(colHtml);
+};
+
+function buildConfig() {
+	return {
+		delimiter: "",
+		newline: "",
+		quoteChar: '"',
+		escapeChar: '"',
+		header: false,
+		transformHeader: undefined,
+		dynamicTyping: false,
+		preview: 0,
+		encoding: "",
+		worker: false,
+		comments: false,
+		step: undefined,
+		complete: undefined,
+		error: undefined,
+		download: false,
+		downloadRequestHeaders: undefined,
+		downloadRequestBody: undefined,
+		skipEmptyLines: false,
+		chunk: undefined,
+		chunkSize: undefined,
+		fastMode: undefined,
+		beforeFirstChunk: undefined,
+		withCredentials: undefined,
+		transform: undefined,
+		delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP]
+	};
+};
+
+function loadData() {
+	Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vQTyhGl_WM_nTFOjDdCrPvmPggXeEWY9Q3WiE_5CJoHtmFS8BPQPayRW-ae-SntRJsqVT96qZobEIV4/pub?gid=2107581036&single=true&output=csv', {
+		download: true,
+		header: true,
+		config,
+		complete: function (results) {
+			allData = results.data;
+			parseData();
+		}
+	});
+};
+
+function parseData() {
+	var $len = allData.length;
+	totalEntries = $len;
+	addTabs();
+	addCards(allData);
+	addCarousel();
+};
+
+const addTabs = () => {
+	const tabs = $('#tabs');
+	tabs.append(`<button class="tab-button selected-tab" onclick="handleTabClick(event, 'all')">ALL <span class="button-total">(${totalEntries})</span></button>`);
+	const categories = [...new Set(allData.map(row => row.category))];
+	let buttonsHtml = '';
+	for (let i = 0; i < categories.length; i++) {
+		const category = categories[i];
+		const buttonLabel = allData.filter(row => row.category === category)[0].button;
+		const categoryCount = filterData(category).length;
+		buttonsHtml += `<button class="tab-button" onclick="handleTabClick(event, '${category}')">${buttonLabel} <span class="button-total">(${categoryCount})</span></button>`;
+	}
+	tabs.append(buttonsHtml);
+};
+
+const handleTabClick = (e, filterBy) => {
+	if (dataFilter !== filterBy) {
+		const selectedTab = e.currentTarget;
+		const allTabs = $('.tab-button');
+		for (let i = 0; i < allTabs.length; i++) {
+			if (allTabs[i] === selectedTab) {
+				allTabs[i].classList.add("selected-tab");
+			} else {
+				allTabs[i].classList.remove("selected-tab");
+			}
+		}
+		filterImages(filterBy);
+		dataFilter = filterBy;
+	}
+}
+
+const filterImages = filterBy => {
+	dataCopy = filterData(filterBy);
+	hideGallery();
+	setTimeout(() => {
+		removeCards();
+		addCards(dataCopy);
+	}, 666);
+	setTimeout(() => {
+		showGallery();
+	}, 666);
+};
+
+const filterData = filterBy => {
+	let dataCopy = [...allData];
+	if (filterBy !== "all") {
+		dataCopy = dataCopy.filter(row => row.category === filterBy);
+	}
+	return dataCopy;
+};
+
+const addCards = data => {
+	let colNum = 0;
+	for (let i = 0; i < data.length; i++) {
+		const col = $('#col-' + colNum);
+		const row = data[i];
+		const htmlString = `
+		<button class="gallery-card ${row.category}"  onclick="selectSlide('${row.image.split(".")[0]}')" data-toggle="modal" data-target="#modal">
+			<img src="images/${row.image}" class="card-image"/>
+			<p class="date">${row.date}</p>
+			<h2 class="caption">${row.caption}</h2>
+		</button>`;
+		col.append(htmlString);
+		colNum = colNum < numCols - 1 ? colNum + 1 : 0;
+	}
+};
+
+const removeCards = () => {
+	const cards = $('.gallery-card');
+	for (let i = cards.length - 1; i >= 0; i--) {
+		const thisCard = cards[i];
+		thisCard.remove();
+	};
+};
+
+const showGallery = () => {
+	imageGallery.removeClass('hide-content');
+	imageGallery.addClass('show-content');
+};
+
+const hideGallery = () => {
+	imageGallery.removeClass('show-content');
+	imageGallery.addClass('hide-content');
+};
+
+const addCarousel = () => {
+	let carouselHtml = '';
+	const carouselInner = $('.carousel-inner');
+	
+	for (let i = 0; i < totalEntries; i++) {
+		const row = allData[i];
+		const imageSource = row.image;
+		const imageId = imageSource.split(".")[0]
+		let slideHtml = `
+		<div class="carousel-item" id="${imageId}">
+			<div class="slide-container">
+				<img class="d-block w-100" src="images/${imageSource}" id="${imageId}-img">
+				<div class="slide-banner">
+					<p class="date">${row.date}</p>
+					<h1>${row.caption}</h1>
+					<p class="desc">${row.desc}</p>
+					<p class="read-more"><em><a href="${row.link}">Read more</a></em></p>
+				</div>
+			</div>
+		</div>`;
+		carouselHtml += slideHtml;
+	}
+	carouselInner.append(carouselHtml);
+};
+
+const selectSlide = selectedImage => {
+	for (let i = 0; i < totalEntries; i++) {
+		const thisID = allData[i].image.split(".")[0];
+		if (thisID === selectedImage) {
+			$(`#${thisID}`).addClass('active');
+		} else {
+			$(`#${thisID}`).removeClass('active');
+		}
+	}
+};
+
+$(document).ready(function () {
+	init();
+});
