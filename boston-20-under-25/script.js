8eb642d4cd7a1bad6d83e27d91dee87efb86d568
msalexford <alefor1@gmail.com>
2023-12-20 07:55:47 -0600

    Link working, filtering issue

--- boston-20-under-25/script.js
+++ boston-20-under-25/script.js
@@ -1,82 +1,43 @@
-// function init() {
-// 	//console.log("ready");
+// Create all data variable globally
+var allData;
 
-// 	config = buildConfig();
-// 	loadData();
-
-// }
-
-// function buildConfig() {
-// 	return {
-// 		delimiter: "",	// auto-detect
-// 		newline: "",	// auto-detect
-// 		quoteChar: '"',
-// 		escapeChar: '"',
-// 		header: false,
-// 		transformHeader: undefined,
-// 		dynamicTyping: false,
-// 		preview: 0,
-// 		encoding: "",
-// 		worker: false,
-// 		comments: false,
-// 		step: undefined,
-// 		complete: undefined,
-// 		error: undefined,
-// 		download: false,
-// 		downloadRequestHeaders: undefined,
-// 		downloadRequestBody: undefined,
-// 		skipEmptyLines: false,
-// 		chunk: undefined,
-// 		chunkSize: undefined,
-// 		fastMode: undefined,
-// 		beforeFirstChunk: undefined,
-// 		withCredentials: undefined,
-// 		transform: undefined,
-// 		delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP]
-// 	};
-// }
-
-// function loadData() {
-
-// 	Papa.parse('https://docs.google.com/spreadsheets/d/1lVWFTJ0Tcqv9LYi3SXf-IUGr57OGJ9bryA_GSDlkpPQ/pub?gid=0&single=true&output=csv', {
-// 		download: true,
-//     	header: true,
-// 		config,
-// 		complete: function(results) {
-// 			console.log("Finished:", results.data);
-// 			allData = results.data;
-// 			parseData();
-// 		}
-// 	});
-
-// }
-
-// Function to fetch JSON data
+// Function to fetch Google Sheet data using Papa Parse
 async function fetchData() {
   try {
-    const response = await fetch('data.json')
-    const data = await response.json()
+    const response = await fetch(
+      'https://docs.google.com/spreadsheets/d/1lVWFTJ0Tcqv9LYi3SXf-IUGr57OGJ9bryA_GSDlkpPQ/export?format=csv&id=1lVWFTJ0Tcqv9LYi3SXf-IUGr57OGJ9bryA_GSDlkpPQ'
+      );
+    const csvData = await response.text();
 
-    // Log the data to the console
-    // console.log(data)
+    // Use Papa Parse to parse the CSV data
+    Papa.parse(csvData, {
+      header: true,
+      complete: function (results) {
+        // Log the parsed data to the console
+        console.log(results.data);
 
-    // Get the container element
-    const container = document.getElementById('card-container')
+        // Get the container element
+        const container = document.getElementById('card-container');
 
-    // Append the cards to the container
-    appendCardsToContainer(container, data)
+        // Append the cards to the container
+        appendCardsToContainer(container, results.data);
+      },
+      error: function (error) {
+        console.error('Error parsing CSV data:', error);
+      }
+    });
   } catch (error) {
-    console.error('Error fetching data:', error)
+    console.error('Error fetching data:', error);
   }
 }
 
 // Call the fetchData function to initiate the process
-fetchData()
+fetchData();
 
 // Function to create a card element
 function createCard(player) {
-  const card = document.createElement('div')
-  card.classList.add('card')
+  const card = document.createElement('div');
+  card.classList.add('card');
 
   // Use the player data to create the card's content
   card.innerHTML = `
@@ -101,33 +62,72 @@ function createCard(player) {
               <a href="${player['link-to-more']}" target="_blank">More about ${player['name-first']} ${player['name-last']}</a>
           </div>
       </div>
-  `
+  `;
 
-  return card
+  return card;
 }
 
 // Function to append cards to the container
 function appendCardsToContainer(container, players) {
-  players.forEach((player, index) => {
-    const card = createCard(player)
-    container.appendChild(card)
-  })
+  // Clear existing cards
+  container.innerHTML = '';
+
+  // Apply sorting and filtering logic
+  const selectedSortOption = document.getElementById('sort').value;
+  const selectedFilterOption = document.getElementById('filter').value;
+
+  players
+    .filter(player => selectedFilterOption === 'all' || player['team'] === selectedFilterOption)
+    .sort((a, b) => {
+      switch (selectedSortOption) {
+        case 'rank':
+          return a['rank'] - b['rank'];
+        case 'name-last':
+          return a['name-last'].localeCompare(b['name-last']);
+        case 'birthdate':
+          return new Date(a['birth-date']) - new Date(b['birth-date']);
+        default:
+          return 0;
+      }
+    })
+    .forEach(player => {
+      const card = createCard(player);
+      container.appendChild(card);
+    });
 }
 
 // Event listener for sorting dropdown
 document.getElementById('sort').addEventListener('change', function () {
-  const selectedSortOption = this.value
-  // Call a function to sort the data based on the selected option
-  // Implement your sorting logic here
-})
+  const selectedSortOption = this.value;
+
+  if (selectedSortOption === 'name-last') {
+    allData.sort((a, b) => a['name-last'].localeCompare(b['name-last']));
+  } else if (selectedSortOption === 'rank') {
+    allData.sort((a, b) => a['rank'] - b['rank']);
+  } else if (selectedSortOption === 'birth-date') {
+    allData.sort((a, b) => new Date(a['birth-date']) - new Date(b['birth-date']));
+  }
+
+  // Re-render the cards in the sorted order
+  const container = document.getElementById('card-container');
+  container.innerHTML = ''; // Clear the container
+  appendCardsToContainer(container, allData);
+});
 
 // Event listener for filtering dropdown
 document.getElementById('filter').addEventListener('change', function () {
-  const selectedFilterOption = this.value
-  // Call a function to filter the data based on the selected option
-  // Implement your filtering logic here
-})
+  const selectedFilterOption = this.value;
 
-$(document).ready(function(){
-	init();
+  // By team
+  const filteredData = selectedFilterOption === 'all'
+    ? allData
+    : allData.filter(player => player['team'] === selectedFilterOption);
+
+  // Re-render the cards with the filtered data
+  const container = document.getElementById('card-container');
+  container.innerHTML = ''; // Clear the container
+  appendCardsToContainer(container, filteredData);
 });
+
+// Initial load of cards
+appendCardsToContainer(document.getElementById('card-container'));
