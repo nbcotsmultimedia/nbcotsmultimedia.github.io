e1b782cf9f7a753fd59c402326456fe9a5898b50
msalexford <alefor1@gmail.com>
2024-01-15 13:16:43 -0600

    Update data link

--- nh-candidate-tracker-v2/js/script.js
+++ nh-candidate-tracker-v2/js/script.js
@@ -43,18 +43,17 @@ function buildConfig() {
 
 function loadData() {
 
-	Papa.parse(
-        'https://docs.google.com/spreadsheets/d/e/2PACX-1vTLf6vvDf-XgnXtguWKz4ayW5P5BCyihNjNpO_axRNLHvaJJUnWS3_TQ0GKcTDBAPPhB0yufGWLX5WE/pub?gid=1672632778&single=true&output=csv', {
-		download: true,
-    	header: true,
-		config,
-		complete: function(results) {
-			//console.log("Finished:", results.data);
-			allData = results.data;
-			parseData();
-
-		}
-	});
+    Papa.parse(
+        'https://docs.google.com/spreadsheets/d/1TtoR-QwRWs7avrZqvAmIfx7BL-cXisMndbR5lgbLGO8/export?format=csv&gid=1672632778', {
+          download: true,
+          header: true,
+          config,
+          complete: function (results) {
+            allData = results.data;
+            parseData();
+          }
+        }
+      );      
 
 }
 
@@ -83,80 +82,75 @@ function parseData() {
 
     var currentDate = new Date();
 
-    for (var i = 0; i < totalEntries; i++) {
-        var dateParts = allData[i].date.split('/');
-        var eventDate = new Date(dateParts[2], dateParts[0] - 1, dateParts[1]);
+    // Helper function to group array of objects by a specified key
+    function groupBy(arr, key) {
+        return arr.reduce(function (acc, obj) {
+            var groupKey = obj[key];
+            acc[groupKey] = acc[groupKey] || [];
+            acc[groupKey].push(obj);
+            return acc;
+        }, {});
+    }
 
-        // Check if eventDate is a valid date
-        if (isNaN(eventDate.getTime())) {
-            console.error("Invalid date for entry at index " + i + ": " + allData[i].date);
-            continue;  // Skip invalid date and move to the next entry
-        }
+    var groupedData = groupBy(allData, 'event_id');
 
-        // Get the day of the week (0 = Sunday, 1 = Monday, etc.)
-        var dayOfWeek = eventDate.getDay();
+    for (var eventId in groupedData) {
+        if (groupedData.hasOwnProperty(eventId)) {
+            var eventGroup = groupedData[eventId];
 
-        if (!currentWeek || !weekStartDate || !weekEndDate || eventDate > weekEndDate) {
-            if (currentWeek) {
-                if (currentWeekHeader) {
-                    $("#content").append(currentWeekHeader);
+            // Create cards for each candidate in the event group
+            eventGroup.forEach(function (eventData) {
+                var dateParts = eventData.date.split('/');
+                var eventDate = new Date(dateParts[2], dateParts[0] - 1, dateParts[1]);
+
+                // Check if eventDate is a valid date
+                if (isNaN(eventDate.getTime())) {
+                    console.error("Invalid date for entry with event_id " + eventId + ": " + eventData.date);
+                    return;  // Skip invalid date and move to the next entry
                 }
-                $("#content").append(currentWeek);
-            }
 
-            weekStartDate = new Date(eventDate);
-            weekStartDate.setDate(eventDate.getDate() - eventDate.getDay());
+                // Get the day of the week (0 = Sunday, 1 = Monday, etc.)
+                var dayOfWeek = eventDate.getDay();
 
-            weekEndDate = new Date(weekStartDate);
-            weekEndDate.setDate(weekStartDate.getDate() + 6);
+                if (!currentWeek || !weekStartDate || !weekEndDate || eventDate > weekEndDate) {
+                    if (currentWeek) {
+                        if (currentWeekHeader) {
+                            $("#content").append(currentWeekHeader);
+                        }
+                        $("#content").append(currentWeek);
+                    }
 
-            currentWeekHeader = $('<h1 class="custom-header">' + formatDate(weekStartDate) + ' - ' + formatDate(weekEndDate) + '</h1>');
-            currentWeek = $('<div class="week-container"></div>');
+                    weekStartDate = new Date(eventDate);
+                    weekStartDate.setDate(eventDate.getDate() - eventDate.getDay());
+
+                    weekEndDate = new Date(weekStartDate);
+                    weekEndDate.setDate(weekStartDate.getDate() + 6);
+
+                    currentWeekHeader = $('<h1 class="custom-header">' + formatDate(weekStartDate) + ' - ' + formatDate(weekEndDate) + '</h1>');
+                    currentWeek = $('<div class="week-container"></div>');
+                }
+
+                if (!currentDay || dayOfWeek !== currentDay.data('day')) {
+                    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                    currentDay = $('<div class="day-container"></div');
+                    currentDay.data('day', dayOfWeek);
+                    currentDay.append("<h2>" + dayNames[dayOfWeek] + " " + eventDate.getDate() + "</h2");
+                    currentWeek.append(currentDay);
+                }
+
+                // Existing code to create cards (similar to your current implementation)
+                var card = $("<div class='event-card'></div");
+
+                // Customize the card content based on eventData
+                card.append("<p>Candidate: " + eventData.candidate + "</p>");
+                card.append("<p>Party: " + eventData.party + "</p>");
+                card.append("<img src='" + eventData.img + "' alt='" + eventData.candidate + "' class='img-fluid' />");
+                // ... (continue with other candidate-specific information)
+
+                // Append the card to the currentDay
+                currentDay.append(card);
+            });
         }
-
-        if (!currentDay || dayOfWeek !== currentDay.data('day')) {
-            const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            currentDay = $('<div class="day-container"></div');
-            currentDay.data('day', dayOfWeek);
-            currentDay.append("<h2>" + dayNames[dayOfWeek] + " " + eventDate.getDate() + "</h2");
-            currentWeek.append(currentDay);
-        }
-
-        var card = $("<div class='event-card'></div");
-
-        if (eventDate.setHours(0, 0, 0, 0) < currentDate.setHours(0, 0, 0, 0)) {
-            card.addClass('past-event');
-        }
-
-        var timeData = allData[i].time.split(' ');
-        var time = $("<div class='time'><p class='time-text'>" + timeData[0] + "</p><p class='am-pm'>" + timeData[1] + "</p></div>");
-
-        var info = $("<div class='info'></div>");
-        info.append(
-            "<p class='name'>" + allData[i].candidate + " (" + allData[i].party[0] + ")" + "</p>",
-            "<p class='type'>" + allData[i].event_type + "</p>",
-            '<div class="address-container">' +
-            '<div class="left-content">' +
-            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#939393" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>' +
-            '</div>' +
-            '<div class="right-content">' +
-            '<p class="address">' +
-            allData[i].address_line_1 + "<br>" +
-            allData[i].address_line_2 + "<br>" +
-            allData[i].city + ", " +
-            allData[i].state + " " +
-            allData[i].zip + "</p>" +
-            '</div>' +
-            '</div>',
-            "<p class='description'>" + allData[i].description + "</p>"
-        );
-
-        var imageContainer = $("<div class='image-container'></div>");
-        var image = $("<img src='" + allData[i].img + "' class='img-fluid' party='" + allData[i].party + "' />");
-
-        card.append(time, info, imageContainer);
-        imageContainer.append(image);
-        currentDay.append(card);
     }
 
     if (currentWeek) {
