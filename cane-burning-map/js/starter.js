f1a741811f7011ecc90bb0bb0783ef0920f1495b
Stogniew <stogniew.a@northeastern.edu>
2024-04-23 09:15:58 -0400

    xtalk fix

--- cane-burning-map/js/starter.js
+++ cane-burning-map/js/starter.js
@@ -28,7 +28,7 @@ let seasons = [[2022, 2023], [2021, 2022]],
 
 function init() {
 	createMap();
-	setTimeout(() => xtalk.signalIframe(), 1000);
+	setTimeout(function(){ xtalk.signalIframe(); }, 1000);
 };
 
 // create D3 map
