ab64993255b4d5bc58733df5f9bee558e55be4a6
Stogniew <stogniew.a@northeastern.edu>
2024-07-01 19:36:28 -0400

    olympic hometowns map

---
+++ olympic-hometowns/app/js/starter.js
@@ -0,0 +1,227 @@
+let ds,
+	totalEntries,
+	allData,
+	mapData,
+	config,
+	usBounds,
+	sports = [],
+	mobile = window.innerWidth < 768,
+	filterVals = {
+		"location_type": [],
+		"sport": []
+	};
+const markerList = [];
+const map = L.map('map', { preferCanvas: true }).setView([44.2423649, -100.8093025], 3);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+	attribution: '©OpenStreetMap, ©CartoDB'
+}).addTo(map);
+const myRenderer = L.canvas({ padding: 0.5 });
+const clusters = new L.MarkerClusterGroup({
+	showCoverageOnHover: false,
+	spiderfyOnMaxZoom: false,
+	disableClusteringAtZoom: 18
+});
+
+const search = e => {
+	const val = e.target.value;
+	$.getJSON('https://nominatim.openstreetmap.org/search.php?q=' + val + '&format=jsonv2&countrycodes=US,CA,PR,VI', function(res) {
+		const response = $("#response");
+		if (res.length > 0) {
+			response.html("")
+			let maxImportance = Math.max(...res.map(r => r.importance))
+			const topRes = res.filter(r => r.importance === maxImportance)[0];
+			map.flyTo([topRes.lat, topRes.lon], 10);
+		} else {
+			response.html("We couldn't find that address, please try another.<br/>Note: It may be helpful to type out the full address, town name or school name.")
+		}
+	});
+};
+
+const addFilters = (locationTypes) => {
+	const filterContainer = $('#legend');
+	let innerHtml = filterContainer.html();
+	for (let i = 0; i < locationTypes.length; i++) {
+		const locationType = locationTypes[i];
+		const color = pickColor(locationType);
+		const id = locationType.replace(/\s+/g, '-').toLowerCase();
+		filterVals["location_type"].push(locationType);
+		const itemHtml = `<div class="form-check form-switch legend-item">
+					<input role="switch" onchange="updateLocationTypes(event, '${color}')" class="form-check-input" type="checkbox" checked id="${id}" value="${locationType}" style="background-color:${color};border-color:${color};">
+					<label class="form-check-label" for="${id}"><p>${locationType}</p></label>
+				</div>`
+		innerHtml += itemHtml;
+	}
+	filterContainer.html(innerHtml);
+};
+
+const updateLocationTypes = (e, color) => {
+	const val = e.target.value;
+	const displayed = filterVals["location_type"];
+	const displayVal = !displayed.includes(val);
+	if (displayVal) {
+		filterVals["location_type"].push(val);
+	} else {
+		filterVals["location_type"] = displayed.filter(el => el != val);
+	}
+	filterData();
+	$(e.target).css("background-color",  displayVal ? color : "#ffffff");
+}
+
+const filterData = () => {
+	mapData = allData.filter(datum => filterVals["location_type"].includes(datum.location_type) && filterVals["sport"].includes(datum.sport));
+	clusters.clearLayers();
+	addClusters(mapData);
+	setTimeout(styleClusters, 50);
+};
+
+const buildSportsList = () => {
+	for (let i = 0; i < allData.length; i++) {
+		const datum = allData[i];
+		const sport = datum.sport;
+		if (!sports.includes(sport)) {
+			sports.push(sport)
+			filterVals["sport"].push(sport);
+		}
+	}
+};
+
+const addSelect = () => {
+	buildSportsList();
+	const select = $('#select');
+	let selectOptions = '<option selected>All sports</option>';
+	for (let i = 0; i < sports.length; i++) {
+		const sport = sports[i];
+		selectOptions += `<option value="${sport}">${sport}</option>`
+	}
+	select.html(selectOptions);
+};
+
+const filterSports = e => {
+	const val = e.target.value;
+	if (val === "All sports") {
+		filterVals["sport"] = sports;
+	} else {
+		filterVals["sport"] = [val];
+	}
+	filterData();
+}
+
+// event handler for marker click
+const handleMarkerClick = marker => {
+	map.flyTo([marker.lat, marker.long], 10);
+};
+
+// fucntion to style clusters
+const styleClusters = () => {
+	const clusters = $(".marker-cluster div");
+	for (let i = 0; i < clusters.length; i++) {
+		const thisCluster = clusters[i];
+		const span = thisCluster.children[0];
+		const numMarkers = parseInt(span.innerHTML);
+		const className = numMarkers < 900 ? numMarkers < 400 ? numMarkers < 200 ? numMarkers < 50 ? numMarkers < 15 ? "xsmall" : "small" : "medium" : "medium-large" : "large" : "xlarge";
+		thisCluster.classList.add(className + "-cluster");
+		span.classList.add(className + "-label");
+	}
+};
+
+const pickColor = locationType => {
+	switch (locationType) {
+		case ("School"):
+			return "rgba(230,0,0,0.65)";
+		case ("Hometown"):
+			return "rgba(230,96,0,0.65)";
+		case ("Current residence"):
+			return "rgba(154,3,30,0.65)";
+	}
+};
+
+// function to add clusters only
+const addClusters = data => {
+
+	for (let i = 0; i < data.length; i++) {
+		const athlete = data[i];
+		clusters.addLayer(L.circleMarker([athlete.lat, athlete.long], {
+			renderer: myRenderer,
+			fillOpacity: 0.65,
+			fillColor: pickColor(athlete.location_type),
+			color: pickColor(athlete.location_type),
+			weight: 0.5,
+			radius: mobile ? 6 : 4
+		})
+			.bindPopup(`<b>${athlete.first_name} ${athlete.last_name}</b>
+		<br/><em>${athlete.sport}</em>
+		<br/>${athlete.location_type}: ${athlete.location_name}`)
+			.openPopup());
+	}
+	map.addLayer(clusters);
+	mapClustered = true;
+};
+
+function init() {
+	//console.log("ready");
+
+	config = buildConfig();
+	loadData('https://docs.google.com/spreadsheets/d/e/2PACX-1vRtdU3ka48OpMBBepDVI7GIdpbpfLNLplzBnVWcIqNIvFtly4rzpeZoiOHjzntW0EqLd1Ed2FGVWc6m/pub?gid=747029840&single=true&output=csv');
+
+};
+
+function buildConfig() {
+	return {
+		delimiter: "",	// auto-detect
+		newline: "",	// auto-detect
+		quoteChar: '"',
+		escapeChar: '"',
+		header: false,
+		transformHeader: undefined,
+		dynamicTyping: false,
+		preview: 0,
+		encoding: "",
+		worker: false,
+		comments: false,
+		step: undefined,
+		complete: undefined,
+		error: undefined,
+		download: false,
+		downloadRequestHeaders: undefined,
+		downloadRequestBody: undefined,
+		skipEmptyLines: false,
+		chunk: undefined,
+		chunkSize: undefined,
+		fastMode: undefined,
+		beforeFirstChunk: undefined,
+		withCredentials: undefined,
+		transform: undefined,
+		delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP]
+	};
+};
+
+
+function loadData(url) {
+
+	Papa.parse(url, {
+		download: true,
+		header: true,
+		config,
+		complete: function (results) {
+			allData = results.data;
+			parseData();
+		}
+	});
+};
+
+function parseData() {
+	mapData = [...allData];
+	addClusters(mapData);
+	styleClusters();
+	addFilters(["Hometown", "School", "Current residence"]);
+	addSelect();
+};
+
+map.on('zoomend', () => {
+	setTimeout(styleClusters, 100);
+})
+
+
+$(document).ready(function () {
+	init();
+});
