b0c2c3fc6b999f2e49b405f109b1a194b713dae8
Nelson Hsu <hsunelson@gmail.com>
2024-07-23 11:13:02 -0400

    profiles update

--- paris-links-generator/js/profiles.js
+++ paris-links-generator/js/profiles.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.ui.dropdown').dropdown();
 
-    d3.csv("https://media.nbcnewyork.com/assets/editorial/national/2022/olympics/athlete-cards/data/athlete-complete-data.csv?" + Math.floor(Math.random() * 1000)).then(function(data) {
+    d3.csv("https://media.nbcnewyork.com/assets/editorial/national/2024/olympics/athlete-cards/data/athlete-complete-data.csv?" + Math.floor(Math.random() * 1000)).then(function(data) {
 
     var data = data.sort(function(x, y){
         return d3.ascending(x.last_name, y.last_name);
@@ -25,15 +25,15 @@ $(document).ready(function() {
       .dropdown('setting', 'onChange', function(d){
         d3.select("#pcode").text(
 
-            '<iframe src="https://data.nbcstations.com/national/2022/olympics/athlete-cards/athlete-cards/?athlete=' + d + '" id="athlete-card-' + d + '" width="100%" height="400" frameborder="0" scrolling="no"></iframe>'
+            '<p><iframe src="https://media.nbcnewyork.com/assets/editorial/national/2024/olympics/athlete-cards/athlete-cards/?athlete=' + d + '" id="athlete-card-' + d + '" width="100%" height="350" frameborder="0" scrolling="no"></iframe></p>'
 
         )
         setTimeout(function() {
             d3.select(".example").html(
-                '<p><link href="https://media.nbcnewyork.com/assets/editorial/national/common/css/mm-embeds-head-foot.css" rel="stylesheet" type="text/css"></p><p><iframe src="https://data.nbcstations.com/national/2022/olympics/athlete-cards/athlete-cards/?athlete=' + d + '" id="athlete-card-' + d + '" width="100%" height="400" frameborder="0" scrolling="no"></iframe></p>'
+                '<p><iframe src="https://media.nbcnewyork.com/assets/editorial/national/2024/olympics/athlete-cards/athlete-cards/?athlete=' + d + '" id="athlete-card-' + d + '" width="100%" height="350" frameborder="0" scrolling="yes"></iframe></p>'
             )
 
-            //xtalk.signalIframe()
+            xtalk.signalIframe()
         }, 1000);
 
 
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
         event.preventDefault();
 
-        var copyText = d3.select(".output");
+        var copyText = d3.select("#pcode");
         navigator.clipboard.writeText(copyText.text());
 
     })
