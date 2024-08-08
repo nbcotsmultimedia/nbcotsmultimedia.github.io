745cc89246a27dcdba9cd3ff107a7d31286625bf
Nelson Hsu <hsunelson@gmail.com>
2024-08-08 17:37:55 -0400

    watch update

--- medals-grid/js/grid.js
+++ medals-grid/js/grid.js
@@ -16,8 +16,13 @@ var sportDropVal = "All"
 function init() {
 	//console.log("ready");
 	$(".pages").hide();
+	$("#vidCon").hide();
 
-	$("#whiteCover").css("height", $(document).height())
+	$("#closeBtn").click(function() {
+		$("#gridCon").css("opacity", 1);
+		$("#vidCon").hide();
+	})
+
 
 	sheets[0] = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFoxPgZ39rGRimNJQxkpPo4Gt3RNxmg0Tw4pxU8kccHAv2ZRXvxpIub6YYHOJzJYoO74Qx5i3MYYse/pub?gid=1439609260&single=true&output=csv";
 
@@ -106,16 +111,28 @@ function generateGrid() {
 
 	for (var i=0; i<totalEntries; i++) {
 
+		var watchTxt;
+		if (dWidth < 500) {
+			watchTxt = "&#9658";
+		}  else {
+			watchTxt = "&#9658 Watch";
+		}
+
 		$("#gridCon").append("<div class='item' id='th" + i + "' data-id='" + i + "' data-gid='" + (i+1) + "' data-color='all_" + allData[i].medal.toLowerCase() + "' data-sport='All_" + allData[i].sport + "' data-tmp='0'>" +
-												"<div class='th'><div class='thcon'><img id='img" + i + "'src='" + allData[i].image + "' width='100%' alt='thumb' onerror='imgError(this);' /></div>" +
+												"<div class='th'><div class='thcon'><img id='img" + i + "'src='" + allData[i].image + "' width='100%' alt='thumb' onerror='imgError(this);' /><div class='watchBtn' data-id='" + i + "'>" + watchTxt +  "</div></div>" +
 												"<p class='pName'>" + allData[i].name + "</p>" +
 												"<p class='pRole'>" + allData[i].medal.toUpperCase() + " - " + allData[i].sport + ", " + allData[i].event + "</p>" +
 												"</div>");
 		//CLICK FUNCTION
-		$("#th" + i).click(function () {
+		/*$("#th" + i).click(function () {
 				//console.log(allData[$(this).attr("data-id")].link);
 				window.open(myparent + allData[$(this).attr("data-id")].link, '_blank');
-		});
+		});*/
+
+		//WATCH BTN
+		if (allData[i].mediatype != "video") {
+			$("#th" + i + " .th .thcon .watchBtn").hide();
+		}
 
 		//COLOR BAND
 		switch (allData[i].medal) {
@@ -129,9 +146,11 @@ function generateGrid() {
 				$("#th" + i + " .th .thcon").css("border-bottom", "5px solid #b4821e")
 				break;
 		}
-
-
 	}
+	
+	$(".watchBtn").click(function() {
+		watchHighlight($(this).attr("data-id"));
+	});
 
 	$(".item").css("margin-right", "0px");
 	//$("#gridCon").css("justify-content", "space-between");
@@ -163,16 +182,16 @@ function resizeItems() {
 			cols = 5;
 		}
 		if (dWidth >= 800) {
-			cols = 6;
+			cols = 5;
 		}
 		if (dWidth >= 1000) {
-			cols = 6;
+			cols = 5;
 		}
 		if (dWidth >= 1200) {
-			cols = 6;
+			cols = 5;
 		}
 		if (dWidth >= 1400) {
-			cols = 8;
+			cols = 5;
 		}
 
 
@@ -181,7 +200,7 @@ function resizeItems() {
 		$(".thcon").css("height", vHeight);
 
 		if (dWidth > 500) {
-			$(".th").css("height", $(".item").width()/3 * 3.0);
+			$(".th").css("height", $(".item").width()/3 * 2.75);
 		} else {
 			$(".th").css("height", $(".item").width()/3 * 4.3);
 		}
@@ -229,7 +248,23 @@ function filterMedals() {
 function showCount() {
 	//$("#showing").html("Showing " + $('.item:visible').length + " items | " + "<a class='shall' href='javascript:showAll()'>Show all</a>");
 	$("#showing").html("Showing " + $('.item:visible').length + " medals");
-	//xtalk.signalIframe();
+	xtalk.signalIframe();
+}
+
+function watchHighlight(which) {
+	$("#gridCon").css("opacity", .2);
+	$("#vidCon").show();
+
+	if ($(document).height() - $("#th" + which).position().top  < 400) {
+		$("#vidCon").css("top", $("#th" + which).position().top -300)
+	} else {
+		$("#vidCon").css("top", $("#th" + which).position().top -20);
+	}
+
+	var theHeight = Math.ceil(($("#vidCon").width() / 16) * 9);
+
+	$("#theVideo").html("<iframe src='" + allData[which].mediaurl + "' width='100%' height='" + theHeight + "' scrolling='no' frameborder='0'></iframe>")
+
 }
 
 $(document).ready(function(){
