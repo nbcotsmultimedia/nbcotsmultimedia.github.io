48604eda1f20cc8d1c9e4435591b957d2b4d756f
msalexford <alefor1@gmail.com>
2025-03-07 13:07:52 -0600

    remove extra files

--- fed-workers-scroll-map/js/utils/index.js
+++
@@ -1,9 +0,0 @@
-// utils/index.js - Central export for utility functions
-
-// Re-export all utility functions
-export * from "./normalization.js";
-export * from "./utils.js";
-export * from "./data-manager.js";
-
-// If you have more utility files, you'd add them like this:
-// export * from './another-utils-file.js';
