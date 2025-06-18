81e1c3c5b1c672b231ff623431e57e10afc55257
Stogniew <stogniew.a@northeastern.edu>
2025-06-18 13:17:36 -0400

    some final changes to ranked-choice scrolly, comments

--- ranked-choice-scrolly/js/scrolly.js
+++ ranked-choice-scrolly/js/scrolly.js
@@ -22,6 +22,7 @@ const yourCandidateLabel = ["", "", "first choice","first choice", "first choice
 // votes for each candidate on each slide
 const rounds = [
 	"",
+	"",
 	{	
 		"Your candidate": 2,
 		"Candidate 1": 44,
@@ -177,6 +178,7 @@ const circleId = (d, step) => {
 	return circleId;
 };
 
+// function to sort circles in chart so that "your vote" will always be in the same place
 const sortCircles = (a, b) => {
 	if (a.data.name === "Your vote" || a.data.name === "Your candidate") {
 		return -1;
@@ -226,8 +228,6 @@ const updateChart = (stepData, step) => {
 			.sort((a, b) => sortCircles(a, b)));
 	const root = pack(stepData);
 
-	console.log(root)
-
 	// change chart data based on current step
 	// style according to colors for current step
 	d3.selectAll(".vote-circle")
@@ -245,28 +245,38 @@ const updateChart = (stepData, step) => {
 		.attr("cy", d => d.y);
 };
 
-const addWinnerLabel = step => {
+// remove "Winner!" label from chart
+const removeWinnerLabel = () => {
 	d3.select("#ranked-choice-winner")
 		.remove();
-	const vote = d3.select("#winner");
-	const voteRadius = vote.attr("r");
-	const voteX = vote.attr("cx");
-	const voteY = vote.attr("cy") - voteRadius;
+};
 
+// add "Winner!" label to circles when they have more than 50 votes
+const addWinnerLabel = step => {
+	// label moves around between scrollytelling steps, so best to just remove/re-add
+	removeWinnerLabel();
+	
+	// get coordinates/size of winner circle, so label can be placed relative to that circle
+	const winner = d3.select("#winner");
+	const winnerRadius = winner.attr("r");
+	const winnerX = winner.attr("cx");
+	const winnerY = winner.attr("cy") - winnerRadius;
+
+	// set up annotation w/ text, coordinates and colors
 	const annotations = [
 		{
 			note: {
 				label: "Winner!"
 			},
-			x: voteX,
-			y: voteY,
+			x: winnerX,
+			y: winnerY,
 			dy: mobile ? -20 : -30,
 			dx: mobile ? -10 : -20,
 			color: step === 10 ? "#95b85a" : "#79919c"
 		}
 	]
 
-	// Add annotation to the chart
+	// add annotation to chart
 	const makeAnnotations = d3.annotation()
 		.annotations(annotations)
 	svg.append("g")
@@ -274,137 +284,166 @@ const addWinnerLabel = step => {
 		.call(makeAnnotations)
 };
 
-const removeWinnerLabel = () => {
-	d3.select("#ranked-choice-winner")
-		.remove();
-};
-
-const addLegend = step => {
-	if (step === 2 || step === 5 || step === 7) {
-		d3.select("#ranked-choice-legend")
-			.remove();
-
-		setTimeout(() => {
-			const legend = svg.append("g")
-				.attr("id", "ranked-choice-legend");
-
-			legend.append("circle")
-				.attr("cx", mobile ? 10 : 10)
-				.attr("cy", mobile ? -10 : -20)
-				.attr("r", 12)
-				.attr("fill", yourCandidateColors[Math.min(step, yourCandidateColors.length - 1)])
-				.attr("fill-opacity", 0.5)
-				.attr("stroke", yourCandidateColors[Math.min(step, yourCandidateColors.length - 1)])
-				.attr("stroke-width", 0.75);
-
-			legend.append("text")
-				.attr("x", mobile ? 28 : 28)
-				.attr("y", mobile ? -5 : -15)
-				.attr("fill", "#3d3d3d")
-				.style("font-size", "14px")
-				.text(`Your ${yourCandidateLabel[Math.min(step, yourCandidateLabel.length - 1)]}`);
-
-			legend.append("circle")
-				.attr("cx", mobile ? 200 : 190)
-				.attr("cy", mobile ? -10 : -20)
-				.attr("r", 12)
-				.attr("fill", "#79919c")
-				.attr("fill-opacity", 0.5)
-				.attr("stroke", "#79919c")
-				.attr("stroke-width", 0.75);
-
-			legend.append("text")
-				.attr("x", mobile ? 218 : 208)
-				.attr("y", mobile ? -5 : -15)
-				.attr("fill", "#3d3d3d")
-				.style("font-size", "14px")
-				.text("Other candidates");
-		}, 150)
-	} else if (step === 2) {
-		d3.select("#ranked-choice-legend")
-			.remove();
-	} else if (step === 11) {
-		d3.select("#ranked-choice-legend")
-			.remove();
-
-		setTimeout(() => {
-			const legend = svg.append("g")
-				.attr("id", "ranked-choice-legend");
-
-			legend.append("circle")
-				.attr("cx", 10)
-				.attr("cy", -20)
-				.attr("r", 12)
-				.attr("fill", "#79919c")
-				.attr("fill-opacity", 0.5)
-				.attr("stroke", "#79919c")
-				.attr("stroke-width", 0.75);
-
-			legend.append("text")
-				.attr("x", 28)
-				.attr("y", -15)
-				.attr("fill", "#3d3d3d")
-				.style("font-size", "14px")
-				.text("Other candidates");
-		}, 150)
-	}
-};
-
+// remove legend
 const removeLegend = () => {
 	d3.select("#ranked-choice-legend")
 			.remove();
 };
 
+// add legend that includes "your candidate" (relevant for slides 2 through 10)
+const addYourCandidateLegend = step => {
+	// remove old legend if there is one
+	removeLegend();
+
+	// need to wait for step to update before we use it for legend colors/labels
+	setTimeout(() => {
+		// add legend container
+		const legend = svg.append("g")
+			.attr("id", "ranked-choice-legend");
+
+		// add circle for "your candidate"
+		legend.append("circle")
+			.attr("cx", mobile ? 10 : 10)
+			.attr("cy", mobile ? -10 : -20)
+			.attr("r", 8)
+			.attr("fill", yourCandidateColors[Math.min(step, yourCandidateColors.length - 1)])
+			.attr("fill-opacity", 0.5)
+			.attr("stroke", yourCandidateColors[Math.min(step, yourCandidateColors.length - 1)])
+			.attr("stroke-width", 0.75);
+
+		// add label for "your candidate"
+		legend.append("text")
+			.attr("x", mobile ? 20 : 20)
+			.attr("y", mobile ? -5 : -15)
+			.attr("fill", "#3d3d3d")
+			.style("font-size", "13px")
+			.text(`Your ${yourCandidateLabel[Math.min(step, yourCandidateLabel.length - 1)]}`);
+
+		// add circle for "other candidates"
+		legend.append("circle")
+			.attr("cx", mobile ? 160 : 160)
+			.attr("cy", mobile ? -10 : -20)
+			.attr("r", 8)
+			.attr("fill", "#79919c")
+			.attr("fill-opacity", 0.5)
+			.attr("stroke", "#79919c")
+			.attr("stroke-width", 0.75);
+
+		// add label for "other candidates"
+		legend.append("text")
+			.attr("x", mobile ? 170 : 170)
+			.attr("y", mobile ? -5 : -15)
+			.attr("fill", "#3d3d3d")
+			.style("font-size", "13px")
+			.text("Other candidates");
+	}, 150)
+};
+
+// add legend with only "other candidates" (relevant for step 11)
+const addOtherCandidatesLegend = step => {
+	// for last slide, we just want "other candidates" in the legend
+		// remove legend if there is one
+		removeLegend();
+
+		// wait for step to load
+		setTimeout(() => {
+			// add legend container
+			const legend = svg.append("g")
+				.attr("id", "ranked-choice-legend");
+
+			// add "other candidates" circle
+			legend.append("circle")
+				.attr("cx", 10)
+				.attr("cy", -20)
+				.attr("r", 8)
+				.attr("fill", "#79919c")
+				.attr("fill-opacity", 0.5)
+				.attr("stroke", "#79919c")
+				.attr("stroke-width", 0.75);
+
+			// add "other candidates" label
+			legend.append("text")
+				.attr("x", 20)
+				.attr("y", -15)
+				.attr("fill", "#3d3d3d")
+				.style("font-size", "13px")
+				.text("Other candidates");
+		}, 150)
+};
+
+// generate a list of votes for a given candidate
 const voteList = (candidate, votes) => {
 	if (candidate === "Your candidate") {
+		// if this is "your candidate," need to add one "your vote"
 		let voteList = [{ name: `Your vote`, value: 1 }];
+		// then add generic votes
 		voteList = voteList.concat(Array(votes[candidate] - 1).fill().map(item => ({ name: `Generic vote`, value: 1 })));
 		return voteList;
 	} else {
+		// if it's any other candidate, just add generic votes
 		return Array(votes[candidate]).fill().map(item => ({ name: `Generic vote`, value: 1 }));
 	}
 };
 
+// format data point for a given candidate, with candidate name and respective votes
 const candidateVotes = (candidate, votes) => {
+	// generate candidate vot list
 	const candidateChildren = voteList(candidate, votes);
 	return { name: candidate, children: candidateChildren };
-}
+};
 
+// generate data for a given scrollytelling step, so that each candidate has a list of votes and your candidate is differentiated
 const stepData = step => {
-	const votes = rounds[step - 1];
-	const candidates = Object.keys(votes);
+	const votes = rounds[step]; // get votes for this step
+	const candidates = Object.keys(votes); // get candidate names for this step
+	// generate data
 	let newData = {};
 	newData = { name: "outer layer", children: [{ name: "candidates", children: candidates.map(candidate => candidateVotes(candidate, votes)) }] };
 	return newData;
 };
 
+// coordinates for "your vote" annotation
 const annotationCoords = (x, y, radius) => {
 	let coords = [x - (radius*2/3), y - (radius*2/3)];
 	return coords;
 };
 
+// x and y offset for "your vote" annotation
 const annotationOffset = (x, y) => {
 	let offset = [];
-	let xOffset = x > chartWidth / 2 ? chartWidth / 10 : -chartWidth / 3.5;
-	let yOffset = y > chartWidth / 2 ? -chartWidth / 2 : -chartWidth / 3.5;
+	let xOffset = mobile ? -chartWidth / 4.25 : -chartWidth / 3.5;
+	let yOffset = mobile ? -chartWidth / 3 : -chartWidth / 3.5;
 	offset = [xOffset, yOffset];
 	return offset;
 };
 
-const addAnnotation = () => {
+// remove "Your vote annotation"
+const removeYourVoteAnnotation = () => {
 	d3.select("#ranked-choice-annotation")
 		.remove();
+};
+
+// add "Your vote" annotation to chart
+const addYourVoteAnnotation = () => {
+	// if it already exists, remove it
+	d3.select("#ranked-choice-annotation")
+		.remove();
+
+	// get "your vote" circle dimensions/coordinates to position annotation accordingly
 	const vote = d3.select("#your-vote");
 	const voteRadius = parseFloat(vote.attr("r"));
 	const voteX = parseFloat(vote.attr("cx"));
 	const voteY = parseFloat(vote.attr("cy"));
+	// calculate annotation coordinates
 	const coords = annotationCoords(voteX, voteY, voteRadius);
 	const annotationX = coords[0];
 	const annotationY = coords[1];
+	// calculate annotation offset 
 	const offset = annotationOffset(voteX, voteY);
 	const offsetX = offset[0];
 	const offsetY = offset[1];
 
+	// set up annotation with text, coordinates, offset and color
 	const annotations = [
 		{
 			note: {
@@ -418,6 +457,7 @@ const addAnnotation = () => {
 		}
 	];
 
+	// add annotation to chart
 	const makeAnnotations = d3.annotation()
 		.annotations(annotations);
 	svg.append("g")
@@ -425,57 +465,86 @@ const addAnnotation = () => {
 		.call(makeAnnotations);
 };
 
-const removeAnnotation = () => {
-	d3.select("#ranked-choice-annotation")
-		.remove();
-};
-
+// set up scrollytelling  
 const scrollytelling = () => {
-	setTimeout(() => { // set up scrollama scroller
+	// wait to make sure everything has loaded
+	setTimeout(() => { 
+		// scrollama scroller
 		const scroller = scrollama();
 		scroller.setup({
-			step: ".ranked-choice-slide",
-			offset: 0.5
+			step: ".ranked-choice-slide", // each "step" is a div whose class is ".ranked-choice-slide"
+			offset: 0.5 // when 50% of the div is visible, the step changes
 		});
 
 		// event handler for scrolling through a step
 		const handleStepEnter = response => {
-			const slideNum = response.index;
-			addLegend(slideNum);
-			if (slideNum !== 0 && slideNum !== 1 && slideNum !== 3 && slideNum !== 4 && slideNum !== 6) {
+			const slideNum = response.index; // current "slide"/ "step"
+
+			// at different steps, call different functions
+
+
+			if (slideNum !== 1 && slideNum !== 3 && slideNum !== 4 && slideNum !== 6) {
+				// for steps where the chart should change, generate new data and update chart
 				const data = slideNum === 0 ? allData : stepData(slideNum);
 				updateChart(data, slideNum);
 			}
 
+			// on first slide, remove legend if it exists and add "Your vote" annotation
 			if (slideNum === 0) {
-				setTimeout(() => addAnnotation(), 600);
+				setTimeout(() => addYourVoteAnnotation(), 600);
 				removeLegend();
 			}
 
-			if (slideNum === 2) {
-				removeAnnotation();
+			// remove legend when scrolling back to first chart
+			if (slideNum === 1) {
+				removeLegend();
 			}
 
+			// remove "Your vote" annotation after first chart
+			if (slideNum === 2) {
+				removeYourVoteAnnotation();
+			}
+
+			// add legend for charts on steps 2 through 10
+			if (slideNum === 2 || slideNum === 5 || slideNum === 7) {
+				addYourCandidateLegend();
+			}
+
+			// remove "Winner!" label if scrolling backwards before step 9
+			if (slideNum === 8) {
+				removeWinnerLabel();
+			}
+
+			// add "Winner!" annotation for steps 9 through 11
 			if (slideNum >= 9) {
 				setTimeout(() => addWinnerLabel(slideNum), 500);
 			}
 
-			if (slideNum === 8) {
-				removeWinnerLabel();
+			// add legend for step 11
+			if (slideNum === 11) {
+				addOtherCandidatesLegend();
 			}
 		};
+
+		// add event handler for step enter
 		scroller.onStepEnter(handleStepEnter);
 	}, 200);
 };
 
+// initialize page
 function init() {
+	// generate data for first chart
 	setInitialData();
+	// wait for data to load
 	setTimeout(() => {
+		// create chart
 		makeChart(allData, 0);
+		// initailize scrollytelling
 		scrollytelling();
 	}, 200);
 };
 
+// when document loads, initialize page
 $(document).ready(function () {
 	init();
 });
